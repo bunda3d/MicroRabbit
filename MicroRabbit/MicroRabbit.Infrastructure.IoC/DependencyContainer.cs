b2cc8c1a835f68124dcc95a63960e7bc04eb5e9b
Kris Bunda <bunda3@yahoo.com>
2021-09-12 17:44:56 -0500

    updated DependencyContainer class for Inversion of Control library

---
+++ MicroRabbit/MicroRabbit.Infrastructure.IoC/DependencyContainer.cs
@@ -0,0 +1,19 @@
+﻿using MicroRabbit.Domain.Core.Bus;
+using MicroRabbit.Infrastructure.Bus;
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MicroRabbit.Infrastructure.IoC
+{
+	//allows binding of interfaces with 'concrete' types
+
+	public class DependencyContainer
+	{
+		//use method to register the dependency container for other classes/projects/apps to use the services
+		public static void RegisterServices(IServiceCollection services)
+		{
+			//Domain Bus
+			services.AddTransient<IEventBus, RabbitMQBus>();
+		}
+	}
+}
