3ac9fd1585ee4f83535356728e68674f5f598b2f
bunda3d <bunda3@yahoo.com>
2021-12-19 13:39:01 -0600

    further documented RMQ Bus implementation

--- MicroRabbit/MicroRabbit.Infrastructure.Bus/RabbitMQBus.cs
+++ MicroRabbit/MicroRabbit.Infrastructure.Bus/RabbitMQBus.cs
@@ -17,147 +17,180 @@ using RabbitMQ.Client.Events;
 
 namespace MicroRabbit.Infrastructure.Bus
 {
-	//Sealed because don't want other classes extending or inheriting from
-	public sealed class RabbitMQBus : IEventBus
-	{
-		#region Constructor
+  //Sealed because don't want other classes extending or inheriting from
+  public sealed class RabbitMQBus : IEventBus
+  {
+    #region Constructor
 
-		//MediatR used to send msgs & return response data
-		private readonly IMediator _mediator;
+    //MediatR used to send msgs & return response data
+    private readonly IMediator _mediator;
 
-		//Dictionary for KVP list of Event Handlers
-		private readonly Dictionary<string, List<Type>> _handlers;
+    //Dictionary for KVP list of Event Handlers
+    private readonly Dictionary<string, List<Type>> _handlers;
 
-		//List of Event Types
-		private readonly List<Type> _eventTypes;
+    //List of Event Types
+    private readonly List<Type> _eventTypes;
 
-		public RabbitMQBus(IMediator mediator)
-		{
-			_mediator = mediator;
-			_handlers = new Dictionary<string, List<Type>>();
-			_eventTypes = new List<Type>();
-		}
+    public RabbitMQBus(IMediator mediator)
+    {
+      _mediator = mediator;
+      _handlers = new Dictionary<string, List<Type>>();
+      _eventTypes = new List<Type>();
+    }
 
-		#endregion Constructor
+    #endregion Constructor
 
-		public Task SendCommand<T>(T command) where T : Command
-		{
-			return _mediator.Send(command);
-		}
+    public Task SendCommand<T>(T command) where T : Command
+    {
+      return _mediator.Send(command);
+    }
 
-		//method sends in a generic Event
-		public void Publish<T>(T @event) where T : Event
-		{
-			var factory = new ConnectionFactory()
-			{
-				HostName = "localhost"
-			};
-			using (var connection = factory.CreateConnection())
-			using (var channel = connection.CreateModel())
-			{
-				var eventName = @event.GetType().Name;
+    /// <summary>
+    /// Publish method implementation
+    /// </summary>
+    /// <typeparam name="T">Type of Event</typeparam>
+    /// <param name="event">Event</param>
 
-				channel.QueueDeclare(eventName.Trim(), false, false, false, null);
+    //method sends in a generic Event
+    public void Publish<T>(T @event) where T : Event
+    {
+      var factory = new ConnectionFactory() { HostName = "localhost" };
+      using (var connection = factory.CreateConnection())
+      using (var channel = connection.CreateModel())
+      {
+        var eventName = @event.GetType().Name;
 
-				var message = JsonConvert.SerializeObject(@event);
-				var body = Encoding.UTF8.GetBytes(message);
+        //new queue name == 'eventName' value
+        channel.QueueDeclare(eventName.Trim(), false, false, false, null);
 
-				channel.BasicPublish("", eventName, null, body);
-			}
-		}
+        var message = JsonConvert.SerializeObject(@event);
+        var body = Encoding.UTF8.GetBytes(message);
 
-		//for subscribing to Events and by Handler type
-		public void Subscribe<T, TH>()
-			where T : Event
-			where TH : IEventHandler<T>
-		{
-			//T is the event passed in, TH is handler
-			var eventName = typeof(T).Name;
-			var handlerType = typeof(TH);
+        channel.BasicPublish("", eventName, null, body);
+      }
+    }
 
-			//if List does not have this Event type, add it
-			if (!_eventTypes.Contains(typeof(T)))
-			{
-				_eventTypes.Add(typeof(T));
-			}
-			//if Dictionary has no key entry for this Event type, add it, along with associating the key to New List of Type entry as the value pairing
-			if (!_handlers.ContainsKey(eventName))
-			{
-				_handlers.Add(eventName, new List<Type>());
-			}
-			//[eventName] is key in _handlers Dictionary
-			//check if Dictionary already contains Event type key (T) and if yes, get handler value (TH) pairing
-			if (_handlers[eventName].Any(x => x.GetType() == handlerType))
-			{
-				throw new ArgumentException(
-					$"Handler Type {handlerType.Name} already is registered for '{eventName}'", nameof(handlerType));
-			}
+    /// <summary>
+    /// Subscribe method implementation
+    /// </summary>
+    /// <typeparam name="T">Type of Event</typeparam>
+    /// <typeparam name="TH">Type of IEventHandler</typeparam>
+    /// <exception cref="ArgumentException"></exception>
 
-			//assign handler value and add it to this entry ([eventName]) in list of event type keys
-			_handlers[eventName].Add(handlerType);
+    //for subscribing to Events and by Handler type
+    public void Subscribe<T, TH>()
+      where T : Event
+      where TH : IEventHandler<T>
+    {
+      //T is the event passed in, TH is handler
+      var eventName = typeof(T).Name;
+      var handlerType = typeof(TH);
 
-			//after subscription is straightened out, consume msgs (msg = T and T = Events)
-			StartBasicConsume<T>();
-		}
+      //if List does not have this Event type, add it
+      if (!_eventTypes.Contains(typeof(T)))
+      {
+        _eventTypes.Add(typeof(T));
+      }
+      //if Dictionary has no key entry for this Event type, add it, along with associating the key to New List of Type entry as the value pairing
+      if (!_handlers.ContainsKey(eventName))
+      {
+        _handlers.Add(eventName, new List<Type>());
+      }
 
-		private void StartBasicConsume<T>() where T : Event
-		{
-			var factory = new ConnectionFactory()
-			{
-				HostName = "localhost",
-				DispatchConsumersAsync = true
-			};
+      //basic validation:
+      //if any exists where [eventName] (T) is key of kvp list in _handlers Dictionary,
+      //if true, see if handler value pair (TH) already exists there.
+      //if true, throw exception that value already exists for that key.
+      if (_handlers[eventName].Any(x => x.GetType() == handlerType))
+      {
+        throw new ArgumentException(
+          $"Handler Type {handlerType.Name} already is registered for " +
+          $"'{eventName}'", nameof(handlerType));
+      }
 
-			var connection = factory.CreateConnection();
-			var channel = connection.CreateModel();
+      //else: assign handler value and add it to this entry ([eventName]) in list of event type keys
+      _handlers[eventName].Add(handlerType);
 
-			var eventName = typeof(T).Name;
+      //after subscription is straightened out, consume msgs (msg = T and T = Events)
+      StartBasicConsume<T>();
+    }
 
-			channel.QueueDeclare(eventName, false, false, false, null);
+    /// <summary>
+    /// Consume Event messages by name from 'eventName' queue asynchronously
+    /// </summary>
+    /// <typeparam name="T">Event</typeparam>
+    private void StartBasicConsume<T>() where T : Event
+    {
+      var factory = new ConnectionFactory()
+      {
+        HostName = "localhost",
+        DispatchConsumersAsync = true
+      };
 
-			var consumer = new AsyncEventingBasicConsumer(channel);
-			consumer.Received += Consumer_Received;
+      var connection = factory.CreateConnection();
+      var channel = connection.CreateModel();
 
-			channel.BasicConsume(eventName, true, consumer);
-		}
+      var eventName = typeof(T).Name;
 
-		private async Task Consumer_Received(object sender, BasicDeliverEventArgs e)
-		{
-			var eventName = e.RoutingKey;
-			var message = Encoding.UTF8.GetString(e.Body.Span);
+      //which queue and how will it be used
+      channel.QueueDeclare(eventName, false, false, false, null);
 
-			try
-			{
-				await ProcessEvent(eventName, message).ConfigureAwait(false);
-			}
-			catch (Exception ex)
-			{
-			}
-		}
+      var consumer = new AsyncEventingBasicConsumer(channel);
+      consumer.Received += Consumer_Received;
 
-		private async Task ProcessEvent(string eventName, string message)
-		{
-			//Dictionary key is "eventName"
-			if (_handlers.ContainsKey(eventName))
-			{
-				var subscriptions = _handlers[eventName];
-				foreach (var subscription in subscriptions)
-				{
-					//handler is new type of subscription
-					var handler = Activator.CreateInstance(subscription);
-					//keep looping thru until find valid subscription
-					if (handler == null) continue;
-					//loop thru local Dictionary of eventTypes by name
-					var eventType = _eventTypes.SingleOrDefault(t => t.Name == eventName);
-					//deserialize message, eventType obj into event
-					var @event = JsonConvert.DeserializeObject(message, eventType);
-					//all event handlers implement IEventHandler<ofSomeType>, so let's make it a generic type based on "eventType"
-					var concreteType = typeof(IEventHandler<>).MakeGenericType(eventType);
-					//this routes the handlers
-					//take event handler ("concreteType") and get its method, pass it the handler and event
-					await (Task)concreteType.GetMethod("Handle").Invoke(handler, new object[] { @event });
-				}
-			}
-		}
-	}
+      channel.BasicConsume(eventName, true, consumer);
+    }
+
+    /// <summary>
+    /// Event fired when a delivery arrives for the consumer
+    /// </summary>
+    /// <param name="sender">Msg-sending method</param>
+    /// <param name="e">BasicDeliverEventArgs => contains all info about msg</param>
+    /// <returns></returns>
+    private async Task Consumer_Received(object sender, BasicDeliverEventArgs e)
+    {
+      var eventName = e.RoutingKey;
+      var message = Encoding.UTF8.GetString(e.Body.Span);
+
+      try
+      {
+        await ProcessEvent(eventName, message).ConfigureAwait(false);
+      }
+      catch (Exception ex)
+      {
+        //TODO: add logging
+      }
+    }
+
+    /// <summary>
+    /// Process msgs from all subscription feeds, genericize them and route to Microservices
+    /// </summary>
+    /// <param name="eventName">Used to select for subscription instances eventType</param>
+    /// <param name="message">Body of message delivered by Sender</param>
+    /// <returns>new event object</returns>
+    private async Task ProcessEvent(string eventName, string message)
+    {
+      //Dictionary key is "eventName"
+      if (_handlers.ContainsKey(eventName))
+      {
+        var subscriptions = _handlers[eventName];
+        foreach (var subscription in subscriptions)
+        {
+          //handler is new type of subscription
+          var handler = Activator.CreateInstance(subscription);
+          //keep looping thru until find valid subscription
+          if (handler == null) continue;
+          //loop thru local Dictionary of eventTypes by name
+          var eventType = _eventTypes.SingleOrDefault(t => t.Name == eventName);
+          //deserialize message, eventType obj into event
+          var @event = JsonConvert.DeserializeObject(message, eventType);
+          //all event handlers implement IEventHandler<ofSomeType>, so let's make it a generic type based on "eventType"
+          var concreteType = typeof(IEventHandler<>).MakeGenericType(eventType);
+          //this routes the handlers
+          //take event handler ("concreteType") and get its method, pass it the handler and event
+          await (Task)concreteType.GetMethod("Handle").Invoke(handler, new object[] { @event });
+        }
+      }
+    }
+  }
 }
